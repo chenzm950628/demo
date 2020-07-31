90d20aa6d5c07d02782d61bcf64f5a121c8f0e43
陈钊明 <chenzhaoming@none.com>
2020-07-31 17:11:19 +0800

    新建测试接口

--- src/main/java/com/example/spring/boot/demo/command/controller/UserController.java
+++ src/main/java/com/example/spring/boot/demo/command/controller/UserController.java
@@ -6,10 +6,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>Title:用户控制层 </p>
@@ -26,5 +23,11 @@ public class UserController {
     @PostMapping()
     public ApiResult insert(@Validated @RequestBody Test test){
         return ApiResult.success();
+    }
+
+    @GetMapping()
+    @ApiOperation("测试")
+    public ApiResult test(){
+        return ApiResult.success("8000");
     }
 }
